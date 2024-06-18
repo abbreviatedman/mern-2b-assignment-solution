5d300469aa69b1f0c7ad5888344851521ac6a4a2
Colin Jaffe <colin.jaffe@gmail.com>
2024-06-18 16:46:12 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const logger = require("morgan");
+
+const ingredients = require("./data");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  if (Object.keys(req.query).length === 0) {
+    res.status(200).json(ingredients);
+
+    return;
+  }
+
+  const { type } = req.query;
+  if (!type) {
+    res.status(400).json({
+      message: "failure",
+      payload: 'The only valid query parameter accepted is "type".',
+    });
+
+    return;
+  }
+
+  const types = Object.keys(ingredients);
+  if (!types.includes(type)) {
+    res
+      .status(400)
+      .json({ message: "failure", payload: "No type of that name found." });
+
+    return;
+  }
+
+  res.status(200).json({ message: "success", payload: ingredients[type] });
+});
+
+app.post("/:type", (req, res) => {
+  const { name } = req.body;
+  const { type } = req.params;
+  const types = Object.keys(ingredients);
+  if (!name) {
+    res.status(400).json({
+      message: "failure",
+      payload: "Request body must include a name field for a new ingredient.",
+    });
+
+    return;
+  }
+
+  if (!types.includes(type)) {
+    res
+      .status(500)
+      .json({ message: "failure", payload: "No type by that name." });
+
+    return;
+  }
+
+  if (ingredients[type].includes(name)) {
+    res
+      .status(500)
+      .json({ message: "failure", payload: "Ingredient already exists." });
+
+    return;
+  }
+
+  ingredients[type].push(name);
+  res.status(200).json({ message: "success", payload: name });
+});
+
+app.all('*', (_, res) => {
+  res.status(404).json({message: 'failure', payload: 'Resource not found.'})
+})
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
